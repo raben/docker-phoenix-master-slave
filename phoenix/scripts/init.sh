b4acda4cdc9eaf5fcdb12265e6ba5cb83f63a8db
Yuuki.Arita <y_arita@colopl.co.jp>
2016-10-21 17:07:09 +0900

    version up

--- phoenix/scripts/init.sh
+++ phoenix/scripts/init.sh
@@ -3,13 +3,23 @@
 VOLUME_HOME=$HOME/$NAME
 
 echo " -> Installation detected in $VOLUME_HOME"
-echo " -> Installing Phoenix Application"
 mix local.hex --force
-mix archive.install /phoenix_new-1.1.4.ez --force
-yes | mix phoenix.new $VOLUME_HOME --database mysql
+mix local.rebar --force
+
+echo " -> Installing Phoenix Application"
+#mix archive.install /phoenix_new.ez --force
+mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez --force
+
+
+echo " -> Create New Phoenix Application {$NAME}"
+mix phoenix.new $VOLUME_HOME --database mysql
+
+echo " -> Open $VOLUME_HOME"
 cd $VOLUME_HOME
-npm install
 mix deps.get
+npm install
+sudo npm install -g brunch
+
 
 sed -i -r "s/adapter: Ecto.Adapters.MySQL,/adapter: Ecto.Adapters.MySQL,\n  hostname: \"master\",/i" ./config/dev.exs
 sed -i -r "s/username: \"root\",/username: \"$DB_RWUSER\",/i" ./config/dev.exs
